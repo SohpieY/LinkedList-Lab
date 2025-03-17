89b26d842e0cbfa66324e7e0cf49bce35956fd16
SohpieY <tianlinsy2026@student.cis.edu.hk>
2025-03-17 20:07:16 +0800

    notyet

--- src/impl/ListManipulator.java
+++ src/impl/ListManipulator.java
@@ -17,107 +17,318 @@ public class ListManipulator implements IListManipulator {
     @Override
     public int size(ListNode head) {
         // TODO Auto-generated method stub
-        return 0;
+        // Initialize a counter to help with later counting algorithm.
+        int count = 0;
+        // If the current node is not null, then move to the next.
+        while (head != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
     }
 
     @Override
     public boolean isEmpty(ListNode head) {
         // TODO Auto-generated method stub
-        return false;
+        return head == null;
     }
 
     @Override
     public boolean contains(ListNode head, Object element) {
         // TODO Auto-generated method stub
+        while (head != null) {
+            if (head.equals(element)) {
+                return true;
+            }
+            head = head.next;
+        }
         return false;
     }
 
     @Override
     public int count(ListNode head, Object element) {
         // TODO Auto-generated method stub
-        return 0;
+        int count = 0;
+        while (head != null) {
+            if (head.equals(element)) {
+                count++;
+            }
+            head = head.next;
+        }
+        return count;
     }
 
     @Override
     public String convertToString(ListNode head) {
         // TODO Auto-generated method stub
-        return null;
+        StringBuilder result = new StringBuilder();
+        while (head != null) {
+            result.append(head.element);
+            if (head.next != null) {
+                result.append(", ");
+            }
+            head = head.next;
+        }
+        return result.toString();
     }
 
     @Override
     public Object getFromFront(ListNode head, int n) throws InvalidIndexException {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            throw new InvalidIndexException(); // Out of bounds
+        }
+        if (n == 0) {
+            throw new InvalidIndexException();
+        }
+        else {
+            int count = 1;
+            ListNode currentNode = head;
+            while (count != n) {
+                currentNode = currentNode.next;
+                count += 1;
+            }
+            return currentNode.element;
+        }
     }
 
     @Override
     public Object getFromBack(ListNode head, int n) throws InvalidIndexException {
         // TODO Auto-generated method stub
-        return null;
+        ListNode currentNode = head.previous;
+        int count = 1;
+        while (count != n) {
+            currentNode = currentNode.previous;
+            count += 1;
+        }
+        return currentNode.element;
     }
 
     @Override
     public boolean equals(ListNode head1, ListNode head2) {
         // TODO Auto-generated method stub
+        if (head1 == null && head2 == null) {
+            return true; // Both lists are empty
+        }
+        if (head1 == null || head2 == null) {
+            return false; // One list is empty
+        }
+
+        ListNode current1 = head1;
+        ListNode current2 = head2;
+
+        do {
+            if (!current1.element.equals(current2.element)) {
+                return false; // Mismatch found
+            }
+            current1 = current1.next;
+            current2 = current2.next;
+
+
+            if (current1 == head1 && current2 == head2) {
+                return true; // Both lists are equal and we've returned to the start
+            }
+        } while (current1 != head1 && current2 != head2);
+
+        // If we exit the loop and they are not both at the start, they are not equal
         return false;
     }
 
     @Override
     public boolean containsDuplicates(ListNode head) {
         // TODO Auto-generated method stub
-        return false;
+        if (head == null) {
+            return false;
+        }
+        if (contains(head.next, head)) {
+            return true;
+        }
+        return containsDuplicates(head.next); // Base case
     }
 
     @Override
     public ListNode addHead(ListNode head, ListNode node) {
         // TODO Auto-generated method stub
-        return null;
+        node.next = head; // New node points to head
+        node.previous = head.previous;
+        head.previous = node;
+        return node; // Node becomes new head
     }
 
     @Override
     public ListNode append(ListNode head1, ListNode head2) {
         // TODO Auto-generated method stub
-        return null;
+        if (head1 == null) {
+            return head2;
+        }
+        if (head2 == null) {
+            return head1;
+        }
+
+        ListNode current = head1;
+
+        while (current.next != null && current.next != head1) {
+            current = current.next;
+        }
+
+        current.next = head2;
+
+        return head1;
     }
 
     public ListNode insert(ListNode head, ListNode node, int n) throws InvalidIndexException {
         // TODO Auto-generated method stub
-        return null;
+        if (n == 0) {
+            System.out.println("Error");
+            throw new InvalidIndexException();
+        }
+        if (n == 1) {
+            return addHead(head, node);
+        }
+        if (head == null) {
+            throw new InvalidIndexException();
+        }
+        int count = 1;
+        ListNode currentNode = head;
+        while (count != n) {
+            currentNode = currentNode.next;
+            count += 1;
+        }
+        return addHead(currentNode, node);
     }
 
     public ListNode delete(ListNode head, Object elem) {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            return null; // If the list is empty, return null
+        }
+
+        ListNode currentNode = head;
+        do {
+            if (currentNode.element.equals(elem)) {
+                // Case 1: Deleting the head node
+                if (currentNode == head) {
+                    // If there's only one node
+                    if (currentNode.next == head) {
+                        return null; // List will be empty after deletion
+                    }
+                    // Find the last node to update its next reference
+                    ListNode lastNode = head;
+                    while (lastNode.next != head) {
+                        lastNode = lastNode.next;
+                    }
+                    // Update head
+                    head = head.next;
+                    lastNode.next = head; // Connect last node to new head
+                    head.previous = lastNode; // Update new head's previous reference
+                } else {
+                    // Case 2: Deleting a middle or tail node
+                    ListNode prev = currentNode.previous;
+                    ListNode next = currentNode.next;
+                    prev.next = next;
+                    if (next != null) {
+                        next.previous = prev;
+                    }
+                }
+                return head;
+            }
+            currentNode = currentNode.next;
+        } while (currentNode != head);
+
+        return head;
     }
 
     @Override
     public ListNode reverse(ListNode head) {
         // TODO Auto-generated method stub
-        return null;
+        ListNode temp = null;
+        ListNode current = head;
+
+        while (current != null) {
+            temp = current.previous;
+            current.previous = current.next;
+            current.next = temp;
+            current = current.previous;
+        }
+
+        if (temp != null) {
+            return temp.previous; // Return the new head of the reversed list
+        } else {
+            return head; // Return the original head if the list is empty or has one node
+        }
     }
 
     @Override
     public ListNode split(ListNode head, ListNode node) throws InvalidListException {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null || node == null) {
+            throw new InvalidListException();
+        }
+
+        ListNode secondHead = node.next; // Store the head of the second part
+        node.next = head; // Link the node to the head
+
+        return secondHead; // Return the second part
     }
 
     @Override
     public ListNode map(ListNode head, IMapTransformation transformation) {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            return null;
+        }
+
+        ListNode newHead = null;
+        ListNode lastNode = null;
+
+        for (ListNode current = head; current != null; current = current.next) {
+            ListNode newNode = new ListNode((Integer) transformation.transform(current.element)); // Create a new node
+            if (newHead == null) {
+                newHead = newNode;
+            } else {
+                lastNode.next = newNode;
+            }
+            lastNode = newNode;
+        }
+
+        return newHead;
     }
 
     @Override
     public Object reduce(ListNode head, IReduceOperator operator, Object initial) {
         // TODO Auto-generated method stub
-        return null;
+        Object result = initial;
+
+        for (ListNode current = head; current != null; current = current.next) {
+            result = operator.operate(result, current.element);
+        }
+
+        return result;
     }
 
     @Override
     public ListNode filter(ListNode head, IFilterCondition condition) {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            return null; // Return null for an empty list
+        }
+
+        ListNode newHead = null;
+        ListNode lastNode = null;
+
+        for (ListNode current = head; current != null; current = current.next) {
+            if (condition.isSatisfied(current.element)) {
+                ListNode newNode = new ListNode(current.element);
+                if (newHead == null) {
+                    newHead = newNode;
+                } else {
+                    lastNode.next = newNode;
+                }
+                lastNode = newNode;
+            }
+        }
+
+        return newHead;
     }
 
 
--- src/impl/RecursiveListManipulator.java
+++ src/impl/RecursiveListManipulator.java
@@ -17,110 +17,231 @@ public class RecursiveListManipulator implements IListManipulator {
     @Override
     public int size(ListNode head) {
         // TODO Auto-generated method stub
-        return 0;
+        if(head == null){
+            return 0; //base case return the count if the current is empty
+        }
+        return 1 + size(head.next);
+
     }
 
     @Override
     public boolean isEmpty(ListNode head) {
         // TODO Auto-generated method stub
-        return false;
+        return head == null;
     }
 
     @Override
     public boolean contains(ListNode head, Object element) {
         // TODO Auto-generated method stub
-        return false;
+        if (head == null) {
+            return false; // Base case: reached the end of the list without finding the element
+        }
+        if (head.element.equals(element)) {
+            return true; // Found the element
+        }
+        return contains(head.next, element); // Recur for the next node
+
     }
 
     @Override
     public int count(ListNode head, Object element) {
         // TODO Auto-generated method stub
-        return 0;
+        if (head == null) {
+            return 0;
+        }
+        int count = (head.element.equals(element) ? 1 : 0);
+        return count + count(head.next, element);
     }
 
     @Override
     public String convertToString(ListNode head) {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            return ""; // Base case: empty list
+        }
+        StringBuilder result = new StringBuilder(head.element.toString());
+        if (head.next != null) {
+            result.append(", ").append(convertToString(head.next));
+        }
+        return result.toString();
     }
 
     @Override
     public Object getFromFront(ListNode head, int n) throws InvalidIndexException {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            throw new InvalidIndexException(); // Out of bounds
+        }
+        if (n == 0) {
+            return head.element; // Return the element if n is 0
+        }
+        return getFromFront(head.next, n - 1);
     }
 
     @Override
     public Object getFromBack(ListNode head, int n) throws InvalidIndexException {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            throw new InvalidIndexException();
+        }
+        int size = size(head);
+        if (n >= size) {
+            throw new InvalidIndexException();
+        }
+        return getFromFront(head, size - n - 1);
     }
 
     @Override
     public boolean equals(ListNode head1, ListNode head2) {
         // TODO Auto-generated method stub
-        return false;
+        if (head1 == null && head2 == null) {
+            return true; // Both lists are empty
+        }
+        if (head1 == null || head2 == null) {
+            return false; // One list is empty
+        }
+        if (!head1.element.equals(head2.element)) {
+            return false; // Mismatch in elements
+        }
+        return equals(head1.next, head2.next);
     }
 
     @Override
     public boolean containsDuplicates(ListNode head) {
-        // TODO Auto-generated method stub
-        return false;
+        return containsDuplicates(head, head);
+    }
+
+    private boolean containsDuplicates(ListNode current, ListNode head) {
+        if (current == null) {
+            return false;
+        }
+
+        ListNode temp = head; // Start from the head for comparison
+        while (temp != current) {
+            if (temp.element.equals(current.element)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+
+        return containsDuplicates(current.next, head);
     }
     
     @Override
     public ListNode addHead(ListNode head, ListNode node) {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            return node;
+        }
+        node.next = head;
+        node.previous = head.previous;
+        head.previous = node;
+        return node;
     }
 
     @Override
     public ListNode append(ListNode head1, ListNode head2) {
         // TODO Auto-generated method stub
-        return null;
+        if (head1 == null) {
+            return head2; // Base case
+        }
+        head1.next = append(head1.next, head2); // Recurse until end
+        return head1;
     }
 
     @Override
     public ListNode insert(ListNode head, ListNode node, int n) throws InvalidIndexException {
         // TODO Auto-generated method stub
-        return null;
+        if (n < 0) {
+            throw new InvalidIndexException();
+        }
+        if (n == 0) {
+            node.next = head;
+            return node;
+        }
+        if (head == null) {
+            throw new InvalidIndexException();
+        }
+        head.next = insert(head.next, node, n - 1);
+        return head;
     }
 
     @Override
     public ListNode delete(ListNode head, Object elem) {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            return null;
+        }
+        if (head.element.equals(elem)) {
+            return head.next;
+        }
+        head.next = delete(head.next, elem);
+        return head;
     }
 
 
     @Override
     public ListNode reverse(ListNode head) {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
     }
 
     @Override
     public ListNode split(ListNode head, ListNode node) throws InvalidListException {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null || node == null) {
+            throw new InvalidListException();
+        }
+        if (node.next == null) {
+            return null;
+        }
+
+        ListNode secondHead = node.next;
+        node.next = head;
+        return secondHead;
     }
 
     @Override
     public ListNode map(ListNode head, IMapTransformation transformation) {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            return null;
+        }
+        ListNode newNode = new ListNode(transformation.transform(head.element));
+        newNode.next = map(head.next, transformation);
+        return newNode;
     }
 
     @Override
     public Object reduce(ListNode head, IReduceOperator operator, Object initial) {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            return initial;
+        }
+        Object newResult = operator.operate(initial, head.element);
+        return reduce(head.next, operator, newResult);
     }
 
     @Override
     public ListNode filter(ListNode head, IFilterCondition condition) {
         // TODO Auto-generated method stub
-        return null;
+        if (head == null) {
+            return null;
+        }
+
+        ListNode newHead = filter(head.next, condition);
+        if (condition.isSatisfied(head.element)) {
+            ListNode newNode = new ListNode(head.element);
+            newNode.next = newHead;
+            return newNode;
+        }
+        return newHead;
     }
 
 }
